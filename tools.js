04cdb048d905a94360c2bb5cb335c27123e2e160
Dawson Moore <moore800@purdue.edu>
2021-05-13 23:36:12 -0500

    Initial commit

---
+++ tools.js
@@ -0,0 +1,7 @@
+var tools = {
+    clear: function() {
+        console.log("<script>angular.element(document.getElementsByClassName('fa fa-trash ng-scope')[0].parentNode).scope().Console.clear()</script>");
+    }
+}
+
+module.exports = tools;
